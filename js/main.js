41cb98c695f17ddf0cdf0e746f12387e42c95f5e
田中一彦 <kzhk.tnk@gmail.com>
2019-12-30 21:50:51 +0900

    first commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+/*
+* TOKYO AGAINST RACISM
+* copyright 2019 tanakazuhiko
+*/
+var width, height;
+
+// init
+init();
+
+// init
+function init() {
+    width = window.innerWidth;
+    height = window.innerHeight;
+}
